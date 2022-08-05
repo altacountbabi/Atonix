18143a60984b0aa4bb58576130841863a024b86f
Whoman <82091823+altacountbabi@users.noreply.github.com>
2022-08-05 19:04:04 +0300

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -9,8 +9,6 @@ module.exports = {
    async execute(client, message, args) {
         const user = message.member
         if (user.roles.cache.has(config.roles.dev) || user.roles.cache.has(config.roles.owner)) {
-            const reason = args.join(' ')
-            console.log(reason)
             if (!message.mentions.members.first()) {
                 message.channel.send({ embeds: [ 
                     new Discord.MessageEmbed()
@@ -25,7 +23,7 @@ module.exports = {
                 new Discord.MessageEmbed()
                     .setColor('RED')
                     .setTitle('Kick')
-                    .setDescription(`${mention.tag} has been kicked`)
+                    .setDescription(`${mention.user.tag} has been kicked`)
             ] })
             mention.send({ embeds: [ 
                 new Discord.MessageEmbed()
